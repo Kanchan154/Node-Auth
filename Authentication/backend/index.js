4f85020a2bb677047ed8feb2d144cb6a29785d01
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2025-12-30 21:40:22 +0530

    Add comment for listening to backend port
    Added a comment to clarify the purpose of the listen method.

--- Authentication/backend/index.js
+++ Authentication/backend/index.js
@@ -32,6 +32,7 @@ if (process.env.NODE_ENV === "production") {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
+// listenning to the backend Port and connecting the database
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running in port ${PORT}`)
